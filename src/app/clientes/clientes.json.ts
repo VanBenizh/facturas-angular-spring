5fd34f085438416c9108acbf37e93c1f396492a4
IVAN <ing.ivangarcia13@gmail.com>
2019-09-17 18:13:27 -0500

    initial commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,8 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[]=[
+  {id:1, nombre:'Andres', apellido:'Guzman', email:'profesor@bolsadeideas.com', createAt:'2019-12-07'},
+  {id:2, nombre:'Mr. Jhon', apellido:'Doe', email:'jhon.doe@bolsadeideas.com', createAt:'2019-12-07'},
+  {id:3, nombre:'Linus', apellido:'Torvalds', email:'linus.torvalds@bolsadeideas.com', createAt:'2019-12-07'},
+  {id:4, nombre:'Rasmus', apellido:'Lerdorf', email:'rasmus.lerdorf@bolsadeideas.com', createAt:'2019-12-07'}
+];
